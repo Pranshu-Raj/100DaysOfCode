0aa28be344359308b4db3d9ac865985aa4c33656
PRANSHU RAJ <pranshuraj828@gmail.com>
2021-11-08 22:00:27 +0530

    changed the tailwind config file(error)
    Some error with the build is now flashing.

---
+++ Day 34/ninjafood/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#e2e2d5',
+          200: '#888883',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
